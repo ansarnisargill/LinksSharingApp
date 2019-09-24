a9b9da6e777ad6a3aca6dc536d928a09c7dad260
Ansar <ansarnisargill@gmail.com>
2019-09-24 08:20:21 -0700

    added cashCheckout

---
+++ Pages/CashCheckout.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BraintreeHttp;
+using LinkShareApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PayPal.Core;
+using PayPal.v1.Payments;
+
+namespace LinkShareApp.Pages
+{
+    public class CashCheckoutModel : PageModel
+    {
+        private readonly LinkAppContext _context;
+        [BindProperty]
+        public Link link { get; set; }
+        public void OnGet(int id)
+        {
+            this.link = _context.Links.First(x => x.ID == id);
+        }
+        public CashCheckoutModel(LinkAppContext context)
+        {
+            _context = context;
+        }
+    }
+}
